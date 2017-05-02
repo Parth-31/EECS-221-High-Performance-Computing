d04fb8ed917d68d64e7eaaa00519f1e26fa1eb58
Parth-31 <parth.rupala@gmail.com>
2017-05-01 23:15:42 -0700

    final commit for hw1

--- hw1/parallel-mergesort.cc
+++ hw1/parallel-mergesort.cc
@@ -1,114 +1,146 @@
-/**
- *  \file parallel-mergesort.cc
- *
- *  \brief Implement your parallel mergesort in this file.
- */
-
 #include <assert.h>
 #include <stdio.h>
+#include <string.h>
+#include <omp.h>
 #include <stdlib.h>
 
 #include "sort.hh"
 
-#define THRESHOLD 1000000
+void mergeSort(keytype* A, keytype* B, int start, int end, int thresold);
+int comp(const void* a, const void* b);
+void pMerge(keytype* A, keytype* B, int start1, int end1, int start2, int end2, int start, int thresold);
+void sMerge(keytype* A, keytype* B, int start1, int end1, int start2, int end2, int start);
 
-void mergeSort(keytype *A, int size, keytype *temp);
-void merge(keytype *left, int sizeLeft, keytype *right, int sizeRight, keytype *temp);
-void pmerge(keytype *left, int sizeLeft, keytype *right, int sizeRight, keytype *temp);
-int binarySearch(keytype *a, int size, keytype key);
-void parallelSort (int N, keytype* A);
-
-void parallelSort (int N, keytype* A)
-{
-  keytype *B = new keytype[N];
-  mergeSort(A, N, B);
+void parallelSort (int N, keytype* A) {
+    keytype* B = new keytype[N];
+    
+    #pragma omp parallel
+    {
+        int noThreads = omp_get_num_threads();
+        
+    #pragma omp single
+        {
+            mergeSort(A, B, 0, N-1, N/noThreads);
+        }
+    }
 }
 
-void mergeSort(keytype *A, int size, keytype *temp)
-{
-  if(size<=THRESHOLD)
-  {
-    sequentialSort(size, A);
-    return;
-  }
-  else
-  {
-    int halfSize = size/2;
-    #pragma omp task
-    mergeSort(A, halfSize, temp);
-    mergeSort(A + halfSize, size-halfSize, temp + halfSize);
-    #pragma omp taskwait
-    pmerge(A, halfSize, A + halfSize, size-halfSize, temp);
-    for(int i = 0; i < size; i++)
-    {
-      A[i] = temp[i];
+void mergeSort(keytype* A, keytype* B, int start, int end, int thresold) {
+    
+    int n = end - start + 1;
+    
+    if ( n <= thresold) {
+        qsort(A + start, n, sizeof(keytype), comp);
+        return;
+    } else {
+        int mid = start + (end - start) / 2;
+        
+        #pragma omp task
+        {
+            mergeSort(A, B, start, mid, thresold);
+        }
+        mergeSort(A, B, mid + 1, end, thresold);
+           
+        #pragma omp taskwait
+        pMerge(A, B, start, mid, mid + 1, end, start, thresold);
+        
+        memcpy(A + start, B + start, (end - start + 1) * sizeof(keytype));
     }
-  }
+    
 }
 
-void merge(keytype *left, int sizeLeft, keytype *right, int sizeRight, keytype *temp)
+int comp(const void* a, const void* b)
 {
-  int i = 0, j = 0, k = 0;
-  while(i<sizeLeft&&j<sizeRight)
-  {
-    if(left[i] < right[j])
-    {
-      temp[k++] = left[i++];
-    }
-    else
-    {
-      temp[k++] = right[j++];
-    }
-  }
-  while(i<sizeLeft)
-  {
-    temp[k++] = left[i++];
-  }
-  while(j<sizeRight)
-  {
-    temp[k++] = right[j++];
-  }
+    return ( *(int*)a - *(int*)b );
 }
 
-void pmerge(keytype *left, int sizeLeft, keytype *right, int sizeRight, keytype *temp)
-{
-  if(sizeLeft <= THRESHOLD || sizeRight <= THRESHOLD)
-  {
-    merge(left, sizeLeft, right, sizeRight, temp);
-  }
-  else
-  {
-    int p = sizeLeft/2;
-    int x = left[p];
-    int q = binarySearch(right, sizeRight, x);
-    #pragma omp task
-    pmerge(left, p, right, q, temp);
-    pmerge(left+p, sizeLeft-p, right+q, sizeRight-q, temp+p+q);
-    #pragma omp taskwait
-  }
+int bSearch(keytype key, keytype* arr, int start, int end) {
+    
+    int low = start;
+    int high = start > (end+1) ? start : (end+1);
+    int mid;
+    
+    while (low < high) {
+        
+        mid = low + (high - low) / 2;
+        
+        if (key <= arr[mid]) {
+            high = mid;
+        } else {
+            low = mid + 1;
+        }
+    }
+    return high;
 }
 
-int binarySearch(keytype *a, int size, keytype key)
-{
-  int low = 0;
-  int high = size-1;
-  while(low<=high)
-  {
-    int mid = (high+low)/2;
-    if(a[mid] == key)
-    {
-      return mid + 1;
-    }
-    else if(a[mid] > key)
-    {
-      high = mid - 1;
-    }
-    else
-    {
-      low = mid + 1;
-    }
-  }
-  return low;
+void pMerge(keytype* A,  keytype* B, int start1, int end1, int start2, int end2, int start, int thresold) {
+    
+    int n1 = end1 - start1 + 1;
+    int n2 = end2 - start2 + 1;
+    
+    if((n1+n2) > thresold){
+        
+        if (n1 < n2) {
+            
+            int temp;
+            
+            temp = start1;
+            start1 = start2;
+            start2 = temp;
+            
+            temp = end1;
+            end1 = end2;
+            end2 = temp;
+            
+            temp = n1;
+            n1 = n2;
+            n2 = temp;
+            
+        }
+        
+        if (n1 == 0) {
+            
+            return;
+            
+        } else {
+            
+            int mid = (start1 + end1) / 2;
+            int idx = bSearch(A[mid], A, start2, end2);
+            int q = start + (mid - start1) + (idx - start2);
+            B[q] = A[mid];
+            
+            #pragma omp task
+            {
+                pMerge(A, B, start1, mid - 1, start2, idx - 1, start, thresold);
+            }
+            pMerge(A, B, mid + 1, end1, idx, end2, q + 1, thresold); 
+            
+            #pragma omp taskwait
+        }
+    } else {
+        sMerge(A, B, start1, end1, start2, end2, start);
+    } 
+    
 }
 
-/* eof */
+
+void sMerge(keytype* A, keytype* B, int start1, int end1, int start2, int end2, int start) {
+    
+    int i = start1, j = start2, k = start;
+    
+    while ( (i <= end1) && (j <= end2) ) {
+        
+        if ( A[i] <= A[j] ) {
+            B[k++] = A[i++];
+        } else {
+            B[k++] = A[j++];
+        }
+    }
+    
+    while (i <= end1) {
+        B[k++] = A[i++];
+    }
+    while (j <= end2) {
+        B[k++] = A[j++];
+    }
+}
